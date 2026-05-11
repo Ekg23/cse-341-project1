874c0a24ed65c3abb409520265f2dddeffffeb60
Ekg23 <geoffreyetu56@gmail.com>
2026-05-11 17:07:28 +0000

    project 1 contact part 1 done

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = require('express').Router();
+const usersController = require('../controllers/users');
+
+router.get('/', usersController.getAll);
+router.get('/:id', usersController.getSingleUser)
+
+module.exports = router
